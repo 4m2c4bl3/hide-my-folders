46c25c8959ebc8b9f6f4267c51c7898b04967016
4m2c4bl3 <9441719+4m2c4bl3@users.noreply.github.com>
2026-01-07 17:56:18 +0000

    easy part done

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,40 @@
+const MOD_NAME = "Hide My Folders";
+const FOLDERS_LIST = "Hidden Folders";
+
 Hooks.once('init', async function() {
 
+  game.settings.register(MOD_NAME, FOLDERS_LIST, {
+    scope: 'world',
+    config: false,
+    type: Array,
+    default: [],
+  });
 });
+Hooks.on("getFolderContextOptions", (_app, menuItems) => {
+    const hiddenFolders = game.settings.get(MOD_NAME, FOLDERS_LIST);
 
-Hooks.once('ready', async function() {
+    menuItems.push({
+        name: "Hide From Players",
+        icon: '<i class="fas fa-bolt"></i>',
+        condition: li => {
+            const id = li.dataset.uuid;
+            return game.user.isGM && hiddenFolders.includes(id);
+        },
+        callback: li => {
+            const id = li.dataset.uuid;
+            game.settings.set(MOD_NAME, FOLDERS_LIST, [...hiddenFolders, id]);
+        }
+    });
 
+    menuItems.push({
+        name: "Show To Players",
+        icon: '<i class="fas fa-bolt"></i>',
+        condition: () => {
+            return game.user.isGM && !hiddenFolders.includes(uniqueId);
+        },
+        callback: li => {
+            const id = li.dataset.uuid;
+            game.settings.set(MOD_NAME, FOLDERS_LIST, [...hiddenFolders.filter(f => f != id)]);
+        }
+    });
 });

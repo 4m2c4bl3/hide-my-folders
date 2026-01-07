686aab28ad74ead044e25d4848f7e8d92e695b98
4m2c4bl3 <9441719+4m2c4bl3@users.noreply.github.com>
2026-01-07 17:56:18 +0000

    easy part done

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,65 @@
-Hooks.once('init', async function() {
+const MOD_NAME = "hide-my-folders";
+const FOLDERS_LIST = "hidden-folders-list";
 
+Hooks.once('init', async () => {
+    game.settings.register(MOD_NAME, FOLDERS_LIST, {
+        scope: 'world',
+        config: false,
+        type: Array,
+        default: [],
+        onChange: () => {
+            void ui.sidebar.render();
+        }
+    });
 });
 
-Hooks.once('ready', async function() {
 
+Hooks.on("renderAbstractSidebarTab", (_app, html) => {
+    const elements = html.querySelectorAll('[data-uuid].directory-item');
+    const isGm = game.user.isGM;
+    elements.forEach(e => {
+        const { uuid } = e.dataset;
+        if (!getHiddenFolders().includes(uuid)) return;
+        if (isGm) {
+            e.classList.add('hide-my-folders-selected-gm')
+        }else{
+            e.remove();
+        }
+    });
+});
+
+const getHiddenFolders = () => game.settings.get(MOD_NAME, FOLDERS_LIST);
+
+Hooks.on("getFolderContextOptions", (_app, menuItems) => {
+    menuItems.push({
+        name: game.i18n.localize(`${MOD_NAME}.add`),
+        icon: '<i class="fa-solid fa-eye-slash"></i>',
+        condition: li => {
+            const folder = li.parentElement;
+            const id = folder.dataset.uuid;
+            const classes = folder.classList;
+            return game.user.isGM && classes.contains('folder') && !getHiddenFolders().includes(id);
+        },
+        callback: li => {
+            const folder = li.parentElement;
+            const id = folder.dataset.uuid;
+            game.settings.set(MOD_NAME, FOLDERS_LIST, [...getHiddenFolders(), id]);
+        }
+    });
+
+    menuItems.push({
+        name: game.i18n.localize(`${MOD_NAME}.remove`),
+        icon: '<i class="fa-solid fa-eye"></i>',
+        condition: li => {
+            const folder = li.parentElement;
+            const classes = folder.classList;
+            const id = folder.dataset.uuid;
+            return game.user.isGM && classes.contains('folder') && getHiddenFolders().includes(id);
+        },
+        callback: li => {
+            const folder = li.parentElement;
+            const id = folder.dataset.uuid;
+            game.settings.set(MOD_NAME, FOLDERS_LIST, [...getHiddenFolders().filter(f => f != id)]);
+        }
+    });
 });

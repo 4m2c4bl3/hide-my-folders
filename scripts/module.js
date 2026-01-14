fd6d3908ab6c857f569544ea85cab9a6495e6839
4m2c4bl3 <9441719+4m2c4bl3@users.noreply.github.com>
2026-01-14 13:45:53 +0000

    test

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,6 @@
 const MOD_NAME = "hide-my-folders";
 const FOLDERS_LIST = "hidden-folders-list";
+const EDITOR_ROLE = "hidden-folders-editor-role";
 
 Hooks.once('init', async () => {
     game.settings.register(MOD_NAME, FOLDERS_LIST, {
@@ -11,16 +12,31 @@ Hooks.once('init', async () => {
             void ui.sidebar.render();
         }
     });
+    game.settings.register(MOD_NAME, EDITOR_ROLE, {
+        name: `${MOD_NAME}.settings.min-role`,
+        hint: `${MOD_NAME}.settings.min-role-hint`,
+        scope: "world",
+        config: true,
+        default: CONST.USER_ROLES.GAMEMASTER,
+        type: Number,
+        choices: {
+            2: "USER.RoleTrusted",
+            3: "USER.RoleAssistant",
+            4: "USER.RoleGamemaster",
+        },
+        onChange: () => {
+            void ui.sidebar.render();
+        }
+    });
 });
 
 
 Hooks.on("renderAbstractSidebarTab", (_app, html) => {
     const elements = html.querySelectorAll('[data-uuid].directory-item');
-    const isGm = game.user.isGM;
     elements.forEach(e => {
         const { uuid } = e.dataset;
         if (!getHiddenFolders().includes(uuid)) return;
-        if (isGm) {
+        if (isAllowedEditor(game.user)) {
             e.classList.add('hide-my-folders-selected-gm')
         }else{
             e.remove();
@@ -29,6 +45,8 @@ Hooks.on("renderAbstractSidebarTab", (_app, html) => {
 });
 
 const getHiddenFolders = () => game.settings.get(MOD_NAME, FOLDERS_LIST);
+const getEditorRole = () => game.settings.get(MOD_NAME, EDITOR_ROLE);
+const isAllowedEditor = (user) => user.role >= getEditorRole()
 
 Hooks.on("getFolderContextOptions", (_app, menuItems) => {
     menuItems.push({
@@ -38,7 +56,7 @@ Hooks.on("getFolderContextOptions", (_app, menuItems) => {
             const folder = li.parentElement;
             const id = folder.dataset.uuid;
             const classes = folder.classList;
-            return game.user.isGM && classes.contains('folder') && !getHiddenFolders().includes(id);
+            return isAllowedEditor(game.user) && classes.contains('folder') && !getHiddenFolders().includes(id);
         },
         callback: li => {
             const folder = li.parentElement;
@@ -54,7 +72,7 @@ Hooks.on("getFolderContextOptions", (_app, menuItems) => {
             const folder = li.parentElement;
             const classes = folder.classList;
             const id = folder.dataset.uuid;
-            return game.user.isGM && classes.contains('folder') && getHiddenFolders().includes(id);
+            return isAllowedEditor(game.user) && classes.contains('folder') && getHiddenFolders().includes(id);
         },
         callback: li => {
             const folder = li.parentElement;
